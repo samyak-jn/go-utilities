3322533c53671562954cb62ce78c32c3c3f1a640
Samyak Jain <samyak.jn11@gmail.com>
2021-10-02 19:04:27 +0530

    Add an utitilty in go which fetches pod restart count, go api for command kubectl get pods
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	utils "go-utilities/utils"
 )
 
@@ -8,11 +9,11 @@ var (
 	kubeconfig = "/home/samjain/.kube/config"
 	master     = ""
 	namespace  = "argocd"
-	podName    = "argocd-server-86dcc9f88f-b64d6"
+	podName    = ""
 )
 
 func main() {
 	utils.Get_pod_info(kubeconfig, master, namespace, podName)
 	// utils.Kubectl_apply()var jsonMap map[string]interface{}
-
+	fmt.Print(utils.PodRestartcount("argocd"))
 }
--- utils/pods-memory-info.go
+++ utils/pods-memory-info.go
@@ -5,11 +5,36 @@ import (
 	// metrics "k8s.io/apimachinery/pkg/apis/metrics/v1"
 	"context"
 	"fmt"
+	"log"
+	"os/exec"
+	"strings"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	metrics "k8s.io/metrics/pkg/client/clientset/versioned"
 )
+
+func PodRestartcount(namespace string) [][]string {
+	var allrestartInfo [][]string
+
+	out, err := exec.Command("kubectl", "get", "pods", "-n", namespace).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := string(out)
+
+	for index, i := range strings.Split(res, "\n") {
+		var restartInfo []string
+
+		if index != 0 && index < len(strings.Split(res, "\n"))-1 {
+			restartInfo = append(restartInfo, strings.Fields(i)[0], strings.Fields(i)[3])
+			allrestartInfo = append(allrestartInfo, restartInfo)
+		}
+	}
+
+	return allrestartInfo
+}
 
 func Get_pod_info(kubeconfig string, master string, namespace string, podName string) {
 	config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
@@ -43,4 +68,15 @@ func Get_pod_info(kubeconfig string, master string, namespace string, podName st
 		fmt.Println(elements.Containers[0].Name, elements.Containers[0].Usage["cpu"].ToUnstructured(), elements.Containers[0].Usage["memory"].ToUnstructured())
 	}
 	fmt.Println("----------------------")
+
+	// fmt.Println(strings.Split(string(out), " "))
+
+	// for _, i := range strings.Split(string(out), "\n") {
+	// 	for _, j := range strings.Split(strings.Trim(i, " "), " ") {
+	// 		fmt.Println(strings.Trim(j, " "))
+	// 	}
+	// }
+
+	//
+
 }
