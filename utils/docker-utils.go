c7129c73c84b76190d388f43ccfa589d1a9fc36f
Samyak Jain <samyak.jn11@gmail.com>
2022-01-26 18:00:00 +0530

    Correct port configurations and init database connection code
    Signed-off-by: Samyak Jain <samyak.jn11@gmail.com>

--- utils/docker-utils.go
+++ utils/docker-utils.go
@@ -3,30 +3,22 @@ package utils
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 	"time"
 
 	"k8s.io/apimachinery/pkg/util/wait"
 )
 
-// func DockerRun(tempDatabaseDir string, tempDBName string, uuid string, dockerName string) {
-// 	dockerContainerID := `docker run --name ` + dockerName + ` \
-// 		-v ` + tempDatabaseDir + `:/var/lib/postgresql/data:Z \
-// 		-e POSTGRES_PASSWORD=gitops \
-// 		-e POSTGRES_DB=` + tempDBName + ` \
-// 		-p 6432:6432 \
-// 		--network gitops-net-` + uuid + ` \
-// 		-d \
-// 		postgres:13 \
-// 		-c log_statement='all' \
-// 		-c log_min_duration_statement=0`
-// 	fmt.Println(dockerContainerID)
-
-// 	fmt.Println(dockerContainerID)
-
-// }
-
 func Docker_apply() {
+
+	// cleanup := func(dockerContainerID string, networkBridge string) {
+	// 	// kill the docker container
+	// 	// rm the docker container
+	// 	// => docker rm -f container id
+	// 	//  remove the network bridge
+	// }
+
 	dockerName := "managed-gitops-postgres-test"
 	dockerNetworkcmd := "docker network create %s"
 	uuid := "sam"
@@ -42,7 +34,7 @@ func Docker_apply() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(out), "`docker create network` command Run Successful!")
+	fmt.Println(string(out), "`docker network create` command Run Successful!")
 
 	tempDatabaseDircmd := "mktemp -d -t postgres-XXXXXXXXXX"
 	s = fmt.Sprintf(tempDatabaseDircmd)
@@ -62,7 +54,7 @@ func Docker_apply() {
 	-v ` + string(tempDatabaseDir) + `:/var/lib/postgresql/data:Z \
 	-e POSTGRES_PASSWORD=gitops \
 	-e POSTGRES_DB=` + tempDBName + ` \
-	-p 6432:6432 \
+	-p 6432:5432 \
 	--network gitops-net-` + uuid + ` \
 	-d \
 	postgres:13 \
@@ -76,22 +68,67 @@ func Docker_apply() {
 			"-v", string(tempDatabaseDir)+":/var/lib/postgresql/data:Z",
 			"-e", "POSTGRES_PASSWORD=gitops",
 			"-e", "POSTGRES_DB="+tempDBName,
-			"-p", "6432:6432",
+			"-p", "6432:5432",
 			"--network", "gitops-net-"+uuid,
 			"-d",
 			"postgres:13",
 			"-c", "log_statement=all",
 			"-c", "log_min_duration_statement=0").Output()
 
+		fmt.Println("Docker Container ID: " + string(dockerContainerID))
 		if errDockerRun != nil {
 			log.Fatal(errDockerRun)
 		}
+		if dockerContainerID == nil {
+			return false, errDockerRun
+		}
+		// check for container status
+		status, _ := exec.Command("docker", "container", "inspect", "-f", "{{.State.Status}}", string(dockerContainerID)).Output()
+		fmt.Println(string(status))
+		if string(status) == "running" {
+			return true, nil
+		}
+		fmt.Println(string(status))
+
 		fmt.Println(string(dockerContainerID), "`docker run` command Run Successful!")
+
 		return true, nil
 	})
-
 	if err != nil {
 		log.Fatal("error in executing docker run command")
 	}
+
+	dbCreatecmd := "PGPASSWORD=gitops psql -h localhost -d %s -U postgres -p 6432 -c select 1"
+	s = fmt.Sprintf(dbCreatecmd, tempDBName)
+
+	fmt.Println("\nRunning: ", s)
+	// To get the output of the command
+	err = wait.Poll(5*time.Second, 2*time.Minute, func() (bool, error) {
+		psqlcmd := exec.Command("psql", "-h", "localhost", "-d", tempDBName, "-U", "postgres", "-p", "6432")
+		psqlcmd.Env = os.Environ()
+		psqlcmd.Env = append(psqlcmd.Env, "PGPASSWORD=gitops")
+
+		psqlErr := psqlcmd.Run()
+
+		fmt.Println(psqlcmd.Env, "\n", psqlErr)
+		if psqlErr != fmt.Errorf("0") {
+			return false, psqlErr
+		}
+		return true, nil
+	})
+
+	// dbActivatecmd := "PGPASSWORD=gitops psql -h localhost -d %s -U postgres -p 6432 -c select 1"
+	// s = fmt.Sprintf(dbActivatecmd, tempDBName)
+
+	// fmt.Println("\nRunning: ", s)
+	// // To get the output of the command
+	// out, err = exec.Command("PGPASSWORD=gitops", "psql", "-h", "localhost", "-d", "%s", "-U", "postgres", "-p", "6432", "-c", "select", "1").Output()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// To print which command is running
+
+	// defer cleanup("dockerContainerID", "gitops-net-"+uuid)
 
 }
